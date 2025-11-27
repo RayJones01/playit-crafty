1d3065256b16db2023fbed85a68bcffbcef2c18a
Reagan Jones <112424803+RayJones01@users.noreply.github.com>
2025-11-26 19:29:05 -0500

    Update playit-crafty-installer.sh
    removed package and cleaned up separate java 21 install

--- playit-crafty-installer.sh
+++ playit-crafty-installer.sh
@@ -24,11 +24,8 @@ warn() {
 # Install :^)
 step "Updating package list and installing dependencies..."
 sudo apt update || error_exit "Failed to update apt repositories."
-sudo apt install -y git python3 python3-dev python3-pip software-properties-common \
+sudo apt install -y git python3 python3-dev python3-pip openjdk-21-jdk openjdk-21-jre \
 || error_exit "Dependency installation failed."
-
-step "Installing Java 21 (OpenJDK 21)..."
-sudo apt install -y openjdk-21-jdk openjdk-21-jre || error_exit "Failed to install Java 21."
 
 step "Creating Crafty user account..."
 if id "crafty" &>/dev/null; then
